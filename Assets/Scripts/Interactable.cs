a62c2e163b031644fc5a86f869790e37825ea304
itkb12 <italuamadeus.12@gmail.com>
2023-02-02 20:57:07 -0400

    02022023

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Interact()
+    {
+        Debug.Log("Interact!");
+    }
+}
