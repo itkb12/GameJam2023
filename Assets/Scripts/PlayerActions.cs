358730ec521d4cc88f2678f018e33154952cf3f1
itkb12 <italuamadeus.12@gmail.com>
2023-02-02 20:56:12 -0400

    02022023

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
+    public LayerMask layer;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
             float interactiveRange = 2f;
-            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactiveRange);
-            foreach (Collider collider in colliderArray)
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactiveRange,layer);
+            if (colliderArray.Length != 0)
             {
-                if (collider.TryGetComponent(out DoorInteractable doorInteractable))
-                {
-                    doorInteractable.Interact();
-                }
+                Debug.Log("Interact");
             }
         }
     }
